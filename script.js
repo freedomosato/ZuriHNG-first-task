2334e2c70dee8f99cb42dc2135c73a647366233d
freedomosato <114444490+freedomosato@users.noreply.github.com>
2023-09-11 15:06:11 +0000

    added digital clock

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
 let a = document.getElementById("currentDayOfTheWeek");
 let b =document.getElementById("currentUTCTime");
+let d = document.getElementById("digitalTime");
+
 
 function daysAndTime(){
 
   let today = new Date();
-  let day = today.getDay();
-  
-  let dayList = ["Sunday", "Monday", "Tuesday",     "Wednesday", "Thursday", "Friday", "Saturday"]
-  let todaysDay = dayList[day]
-  a.innerHTML = "Today is " + todaysDay
 
+  let date = today.getDate()
+  let month = today.getMonth() + 1
+  let year = today.getFullYear()
+  let day = today.getDay();
+  let dayList = ["Sunday", "Monday", "Tuesday",     "Wednesday", "Thursday", "Friday", "Saturday"]
+  let todaysDay = dayList[day];
   
+  if(date<10){
+    date = "0" + date
+  }
+  if(month<10){
+    month = "0" + month
+  }
+  
+  a.innerHTML = todaysDay + ", " + date + "/" + month + "/" + year
+
   let time = today.getTime()
   b.innerHTML = time;
+
+  let h = today.getHours()
+  let m = today.getMinutes()
+  let s = today.getSeconds()
+  let ampm = "am"
+
+  if(h>12){
+    h = h-12
+    ampm = "pm"
+  }
+  if(h<10){
+    h = "0" + h
+  }
+
+  if(m<10){
+    m = "0" + m
+  }
+  if(s<10){
+    s = "0"+s
+  }
+
+  let clock = h + " : " + m + " : " + s + " " + ampm
+  d.innerHTML = clock
+
+
 }
 setInterval(daysAndTime, 100)
